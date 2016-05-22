c548bac083a9cb6deb68cec89a7cab75b31ddd28
System Administrator <root@Mera-MB.local>
2016-05-21 22:40:00 -0500

    update

--- scripts/call_ph.sh
+++ scripts/call_ph.sh
@@ -1,4 +1,4 @@
-for fle in /Users/virpatel/Desktop/pub_stuff/relevant_data/dis2mir_db/*; do
+for fle in /Users/virpatel/Desktop/pub_stuff/relevant_data/dis2mir_db/*.txt; do
 	if [[ $fle != *'DS_STORE'* ]]; then
 		cd /Users/virpatel/Desktop/pub_stuff/ProteinHistorian
 		$(python age_enrichment_analysis.py -l /Users/virpatel/Desktop/pub_stuff/relevant_data/time_tree_dates.txt /Users/virpatel/Desktop/pub_stuff/relevant_data/mirna2age_lst.txt $fle)
