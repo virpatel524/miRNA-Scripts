78d9709ca499582dd194fb32d0538f4314541ed7
virpatel524 <virpatel3@gmail.com>
2016-05-21 22:39:00 -0500

    update

--- scripts/call_ph.sh
+++ scripts/call_ph.sh
@@ -1,7 +1,7 @@
-for fle in "/Users/virpatel/Desktop/pub_stuff/relevant_data/dis2mir_db/*"; do
+for fle in /Users/virpatel/Desktop/pub_stuff/relevant_data/dis2mir_db/*; do
 	if [[ $fle != *'DS_STORE'* ]]; then
 		cd /Users/virpatel/Desktop/pub_stuff/ProteinHistorian
-		$(python -l /Users/virpatel/Desktop/pub_stuff/relevant_data/time_tree_dates.txt /Users/virpatel/Desktop/pub_stuff/relevant_data/mirna2age_lst.txt $fle)
+		$(python age_enrichment_analysis.py -l /Users/virpatel/Desktop/pub_stuff/relevant_data/time_tree_dates.txt /Users/virpatel/Desktop/pub_stuff/relevant_data/mirna2age_lst.txt $fle)
 
 	fi
 done
