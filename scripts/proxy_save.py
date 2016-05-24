6c453bf49c34523070bec900e846bdc911d42e57
System Administrator <root@Mera-MB.local>
2016-05-23 19:56:00 -0500

    update

--- scripts/proxy_save.py
+++ scripts/proxy_save.py
@@ -3,4 +3,6 @@ import csv
 with open('/Users/virpatel/Desktop/proxies.txt') as fle:
 	data = list(csv.reader(fle,delimiter='\t'))
 
-alpha =  ['%s:%s\n' %(a[-1], a[-2]) for index, a in enumerate(data) if index % 2 == 1]
+alpha =  ['%s:%s' %(a[-1], a[-2]) for index, a in enumerate(data) if index % 2 == 1]
+for al in alpha:
+	print al
