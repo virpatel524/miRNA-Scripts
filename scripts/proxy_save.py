78fb13606c79462ec1e862fe5a7797bffe5bfb79
System Administrator <root@Mera-MB.local>
2016-05-23 19:39:00 -0500

    update

--- scripts/proxy_save.py
+++ scripts/proxy_save.py
@@ -3,4 +3,4 @@ import csv
 with open('/Users/virpatel/Desktop/proxies.txt') as fle:
 	data = list(csv.reader(fle,delimiter='\t'))
 
-print ["{'http': '%s:%s'}" %(a[-1], a[-2]) for index, a in enumerate(data) if index % 2 == 1]
+print ["{'https': '%s:%s'}" %(a[-1], a[-2]) for index, a in enumerate(data) if index % 2 == 1]
