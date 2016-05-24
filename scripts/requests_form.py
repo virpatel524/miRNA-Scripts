06784246fec4e65959904169bfb22abe7b025f69
System Administrator <root@Mera-MB.local>
2016-05-23 20:04:00 -0500

    update

--- scripts/requests_form.py
+++ scripts/requests_form.py
@@ -3,4 +3,6 @@ import csv
 with open('/Users/virpatel/Desktop/working_proxies.txt') as fle:
 	data = list(csv.reader(fle,delimiter='\t'))
 
-new_data = ["{'https': '%s'}" %(a) for a in data]
+new_data = ["{'https': '%s'}" %(a[0]) for a in data]
+
+print new_data
