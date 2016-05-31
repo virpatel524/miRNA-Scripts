f7e1376febc84182d196f7272687b19776adc399
System Administrator <root@Mera-MB.local>
2016-05-30 22:25:00 -0500

    update

--- main_script_v2.py
+++ main_script_v2.py
@@ -284,7 +284,7 @@ def family_homogenity(human_mirlst, mirna2disease, mirna2age):
 			vec = generate_disease_vector(dislst, mirna2disease[mir])
 			tmp = pd.DataFrame([vec,],index=[mir,], columns=dislst)
 			print tmp
-			all_mir_vector_df.append(tmp)
+			all_mir_vector_df = all_mir_vector_df.append(tmp)
 	print all_mir_vector_df
 
 
