894ae3630ad4362a1f3c0fba0dfa377a3cae9e19
System Administrator <root@Mera-MB.local>
2016-05-30 21:58:00 -0500

    update

--- main_script_v2.py
+++ main_script_v2.py
@@ -253,12 +253,12 @@ def gen_dis_db(disease2mirna,mirna2age):
 			for mir in disease2mirna[dis]:
 				if mir in mirna2age:
 					cur_disfle.write('%s\n' %(mir))
-					
 
 
 
 	
-
+def family_homogenity(mirlst_by_fam, mirna2disease, mirna2age):
+	print mirlst_by_fam
 
 
 
@@ -283,7 +283,7 @@ def main():
 	taragedb = '/Users/virpatel/Desktop/pub_stuff/ProteinHistorian/example/ages/human_age-label_wagner1.txt'
 	timetreedb = '/Users/virpatel/Desktop/pub_stuff/relevant_data/time_tree_dates.txt'
 
-	# mega_mir_lst, mirlst_by_species, mirlst_by_fam = sort_mir(open(mirdb,'r'),open(famdb,'r'))
+	mega_mir_lst, mirlst_by_species, mirlst_by_fam = sort_mir(open(mirdb,'r'),open(famdb,'r'))
 	mirna2disease, disease2mirna = diseaese_parser(open(diseasedb,'r'))
 	mirna2age, age2mirna = age_parser(open(agedb, 'r'))
 	age2clade, clade2age = time_tree_parse(timetreedb)
@@ -294,9 +294,12 @@ def main():
 
 
 
-	mir_num_dis_bin(mirna2disease, mirna2age,age2clade)
+	# mir_num_dis_bin(mirna2disease, mirna2age,age2clade)
 
-	gen_dis_db(disease2mirna, mirna2age)
+
+	# gen_dis_db(disease2mirna, mirna2age)
+
+	family_homogenity(mirlst_by_fam, mirna2disease, mirna2age)
 
 
 
