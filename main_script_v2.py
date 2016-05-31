807494c5c9a88338bfedcfd84e0900745ed95ca1
System Administrator <root@Mera-MB.local>
2016-05-30 22:46:01 -0500

    update

--- main_script_v2.py
+++ main_script_v2.py
@@ -286,7 +286,7 @@ def family_homogenity(human_mirlst, mirna2disease, mirna2age):
 			all_mir_vector_df = all_mir_vector_df.append(tmp)
 
 	
-	print all_mir_vector_df.index
+	all_mir_vector_df[all_mir_vector_df.index[0]]
 
 
 
