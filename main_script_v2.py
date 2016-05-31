04d0e70214eb7e383ede94ac9364bda07f8317a6
System Administrator <root@Mera-MB.local>
2016-05-31 16:19:00 -0500

    update

--- main_script_v2.py
+++ main_script_v2.py
@@ -306,7 +306,8 @@ def family_homogenity(human_mirlst, mirna2disease, mirna2age):
 		family_avg_hamming.append(mean(family_vector))
 		family_avg_age.append(mean([float(mirna2age[mirna]) for mirna in mirlst if mirna in mirna2age]))
 
-	print spearmanr(family_avg_hamming, family_avg_age)
+	fam_df = pd.DataFrame([family_avg_age,family_avg_hamming],columns=['1','2'])
+	print fam_df
 
 
 		
