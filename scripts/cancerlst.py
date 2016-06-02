39125add33f3e794d22c198fa8eaef511a685342
System Administrator <root@Mera-MB.local>
2016-06-02 13:28:00 -0500

    update

--- scripts/cancerlst.py
+++ scripts/cancerlst.py
@@ -3,11 +3,11 @@ import csv
 data = list(csv.reader(open('/Users/virpatel/projects/vanderbilt-summer-2014/data/list_of_HMDD_diseases.txt','r'),delimiter='\t'))
 canlst = []
 for ind,el in enumerate(data):
-	dis = el[0]
-	print dis
-	if 'oma' in dis or 'eoplas' in dis:
-		print 'hi'
-		canlst.append(dis)
-		data.pop(ind)
+	# dis = el[0]
+	# if dis == 'Glioblastoma':
+	# 	print 'ah'
+	# if 'oma' in dis or 'eoplas' in dis:
+	# 	canlst.append(dis)
+	# 	data.pop(ind)
 
-print data
+# print data
