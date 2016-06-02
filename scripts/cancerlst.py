8958bcaea3c51878478c68ffec62beca5718809d
System Administrator <root@Mera-MB.local>
2016-06-02 13:37:00 -0500

    update

--- scripts/cancerlst.py
+++ scripts/cancerlst.py
@@ -10,7 +10,7 @@ data = flatten(data)
 canlst = []
 nonlst = []
 for ind,dis in enumerate(data):
-	if 'oma' in dis or 'eoplas' in dis or 'Leukemia' in dis or 'Sezary' in dis:
+	if 'oma' in dis or 'eoplas' in dis or 'Leukemia' in dis or 'Sezary' in dis or 'Macroglobulinemia' in dis:
 		canlst.append(dis)
 	else: nonlst.append(dis)
 
