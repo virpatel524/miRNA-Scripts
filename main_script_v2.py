7d099426d2ccf4e9d00d6bafb2081063891631e5
System Administrator <root@Mera-MB.local>
2016-06-02 11:09:00 -0500

    update

--- main_script_v2.py
+++ main_script_v2.py
@@ -442,7 +442,6 @@ def disease_bootstrapping(mirna2age, mirna2disease,mirna2target):
 				while_loop_safety = 0
 				new_ages_lst = []
 				while len(new_ages_lst) != len(age_of_supporting_mir):
-					if 
 					while_loop_safety += 1
 					new_ages_lst = [mirna2age[ran_choice] for ran_choice in random.sample(set(mirna2disease),len(age_of_supporting_mir)) if ran_choice in mirna2age]
 				if float(median(new_ages_lst)) > med_dis: counter_under += 1
