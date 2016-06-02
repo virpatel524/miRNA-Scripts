f517b56356082c92c0644945ab1a57ff98a9612b
System Administrator <root@Mera-MB.local>
2016-06-02 13:27:01 -0500

    update

--- scripts/cancerlst.py
+++ scripts/cancerlst.py
@@ -2,12 +2,12 @@ import csv
 
 data = list(csv.reader(open('/Users/virpatel/projects/vanderbilt-summer-2014/data/list_of_HMDD_diseases.txt','r'),delimiter='\t'))
 canlst = []
-for el in data:
+for ind,el in enumerate(data):
 	dis = el[0]
 	print dis
 	if 'oma' in dis or 'eoplas' in dis:
 		print 'hi'
 		canlst.append(dis)
-		data.remove(el)
+		data.pop(ind)
 
 print data
