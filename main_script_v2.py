93911f9acb0fd6adf1d609934cda06420c299b70
System Administrator <root@Mera-MB.local>
2016-06-02 11:38:00 -0500

    update

--- main_script_v2.py
+++ main_script_v2.py
@@ -459,6 +459,7 @@ def disease_bootstrapping(mirna2age, mirna2disease,mirna2target):
 	for dis in disease2mirna:
 		age_of_supporting_mir = [mirna2age[a] for a  in disease2mirna[dis] if a in mirna2age]
 		z,b  = mannwhitneyu(alldismir, age_of_supporting_mir)
+		print z
 		print b
 		print 'Disease:%s, mann: %s' %(dis, z)
 
