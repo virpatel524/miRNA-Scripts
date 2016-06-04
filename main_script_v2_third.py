4158469cfa03d07fd10b6642384930bb413b9114
System Administrator <root@Mera-MB.local>
2016-06-04 18:08:00 -0500

    update

--- main_script_v2_third.py
+++ main_script_v2_third.py
@@ -398,16 +398,18 @@ def target_gene_expression_analysis(mirna2age, mirna2disease,mirna2family,gene2a
 		mir_age_lst = []
 		exp_val = []
 
-		for mir in mirna2family:
+		totcount = 0
+		sub5
+		for mir in mirna2age:
 			# if len(mirna2family[mir]) < 2: continue
-			for submir in mirna2family[mir]:
-				if submir not in mirna2age or submir not in mir_expdb.index: continue
-				v = float(sum(mir_expdb.loc[submir].tolist()))
-				mirage = mirna2age[submir]
-				mir_age_lst.append(mirage)
+
+			if mir not in mir_expdb.index: continue
+			v = float(sum(mir_expdb.loc[submir].tolist()))
+			mirage = mirna2age[submir]
+			mir_age_lst.append(mirage)
 
 
-				exp_val.append(v)
+			exp_val.append(v)
 
 					# if mirage > 100.0: old_num_tis.append(sum(mir_expdb.loc[mir].tolist()))
 					# else: yung_num_tis.append(sum(mir_expdb.loc[mir].tolist()))
