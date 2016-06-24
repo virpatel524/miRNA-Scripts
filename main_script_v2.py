4b4ceb6e68c9c018ac80a5fcad53e2c148f40b51
System Administrator <root@Mera-MB.local>
2016-06-23 20:35:00 -0500

    update

--- main_script_v2.py
+++ main_script_v2.py
@@ -782,9 +782,9 @@ def main():
 
 	mirna2disease_collapsed =  collapse_cancer_lst(mirna2disease)
 
-	family_homogenity_collapsed(human_mirlst, mirna2disease_collapsed, mirna2age)
+	# family_homogenity_collapsed(human_mirlst, mirna2disease_collapsed, mirna2age)
 
-	mir_num_dis_bin_collapsed(mirna2disease_collapsedf, mirna2age, age2clade)
+	# mir_num_dis_bin_collapsed(mirna2disease_collapsedf, mirna2age, age2clade)
 
 	main_fraction_under_figure(mirna2tar, mirna2age, tar2age)
 
