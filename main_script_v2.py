90b8a91d552cdb1dce07848ba09f75eaaae3aed6
System Administrator <root@Mera-MB.local>
2016-06-23 23:57:01 -0500

    update

--- main_script_v2.py
+++ main_script_v2.py
@@ -685,7 +685,7 @@ def target_gene_expression_analysis(mirna2age, mirna2disease,mirna2family,gene2a
 
 		# for mir in mir_not_in_fam
 
-		lst2 = [fam2allvals[fam][2] for fam in fam2allvals]
+		lst2 = [fam2allvals[fam][3] for fam in fam2allvals]
 		lst1 = [fam2allvals[fam][0] for fam in fam2allvals]
 		plt.scatter(lst1, lst2)
 		plt.show()
