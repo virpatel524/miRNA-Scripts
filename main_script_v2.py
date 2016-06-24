018d417452f9ed29b77c3e5eea69a03baa67530e
System Administrator <root@Mera-MB.local>
2016-06-23 23:28:00 -0500

    update

--- main_script_v2.py
+++ main_script_v2.py
@@ -668,7 +668,7 @@ def target_gene_expression_analysis(mirna2age, mirna2disease,mirna2family,gene2a
 		fam2allvals = {}
 
 		for fam in mirna2family:
-			if len([a for a in mirna2family if a in mir_expdb.index) < 4: continue
+			if len([a for a in mirna2family if a in mir_expdb.index]) < 4: continue
 			lst_ham = []
 
 			for mir in mirna2family[fam]:
@@ -681,6 +681,8 @@ def target_gene_expression_analysis(mirna2age, mirna2disease,mirna2family,gene2a
 
 		lst1 = [fam2allvals[fam][0] for fam in fam2allvals]
 		lst2 = [fam2allvals[fam][0] for fam in fam2allvals]
+		print lst1
+
 
 		print spearmanr(lst1, lst2)
 
