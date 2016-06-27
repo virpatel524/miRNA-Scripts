c4c4e950a51737fe1215ba5f1f0bb1061cc1b0ed
virpatel524 <virpatel3@gmail.com>
2016-06-27 17:04:57 -0500

    update

--- main_script_v2.py
+++ main_script_v2.py
@@ -867,7 +867,9 @@ def main_fraction_under_figure(mirna2tar, mirna2age, target2age):
 def heatmap_analysis(mirna2age, mirna2diseae, mirna2family, gene2age):
 	round_robyn_target = pd.read_csv('/Users/virpatel/Desktop/pub_stuff/relevant_data/mir_target_vectordb.txt', sep='\t',index_col=[0])
 	round_robyn_target = pd.read_csv('/Users/virpatel/Desktop/pub_stuff/relevant_data/mir_target_vectordb.txt', sep='\t',index_col=[0])
-	
+
+
+
 
 
 
@@ -928,6 +930,8 @@ def main():
 
 	# main_fraction_under_figure(mirna2tar, mirna2age, tar2age)
 
+
+	heatmap_analysis(mirna2age, mirna2diseae, mirna2family, gene2age)
 
 
 main()
