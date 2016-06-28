75a45ff6ea3d82bfeddbe17aa55185b65d64593a
System Administrator <root@Mera-MB.local>
2016-06-28 17:30:12 -0500

    update

--- main_script_v3.py
+++ main_script_v3.py
@@ -993,7 +993,8 @@ def heatmap_analysis(mirna2age, mirna2disease, mirna2family, gene2age):
 				mir_nondis_target.append(float(round_robyn_target[alpha]))
 
 
-	sns.violinplot(mir_dis_target)
+	mir_dis_target
+
 
 	sns.plt.show()
 
