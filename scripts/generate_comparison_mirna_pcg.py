6756b6c2ce6c646b30af08e6a557c5f65e17d5d3
virpatel524 <virpatel3@gmail.com>
2016-06-29 17:45:40 -0500

    update

--- scripts/generate_comparison_mirna_pcg.py
+++ scripts/generate_comparison_mirna_pcg.py
@@ -8,8 +8,11 @@ def import_pcg():
 	
 
 def import_human_mirages():
+	data = []	
+
 	with open(sys.argv[1],'r') as mir_ages_fle:
-	data = list(csv.reader(mir_ages_fle,delimiter='\t'))
+		data = list(csv.reader(mir_ages_fle,delimiter='\t'))
+
 	print data
 
 
