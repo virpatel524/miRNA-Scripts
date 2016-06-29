ced2afc49e368fb969252de8d73f0893d32cf372
System Administrator <root@Mera-MB.local>
2016-06-28 20:24:32 -0500

    update

--- main_script_v3.py
+++ main_script_v3.py
@@ -1080,8 +1080,8 @@ def heatmap_analysis(mirna2age, mirna2disease, mirna2family, gene2age):
 	violin_nocomp(mirna2family_edited.keys(), mir_targetdb, 'Family', 'Number of Gene Targets', 'fam_tarnum')
 	violin_nocomp(mirna2family_edited.keys(), mir_expdb, 'Family', 'Number of Tissues Expressed In', 'fam_tisnum')
 
-	violin_comp_norel(mirna2, hamming_df, tipo, xentry, df_name)
-
+	violin_comp_norel(mirna2disease.keys(), round_robyn_target, 'Disease', 'Hamming Target Comparisons', 'dis_tarham')
+	violin_comp_norel(mirna2family_edited.keys(), round_robyn_target, 'Disease', 'Hamming Target Comparisons', 'dis_tarham')
 
 
 
