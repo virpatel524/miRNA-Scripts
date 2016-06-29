5511d3d0953738f9778f28e348e9b6bedde925a8
System Administrator <root@Mera-MB.local>
2016-06-28 20:48:37 -0500

    update

--- main_script_v3.py
+++ main_script_v3.py
@@ -1052,8 +1052,8 @@ def violin_comp_rel(gen_exlus_dic, hamming_df, tipo, xentry, df_name):
 
 	data_master = pd.DataFrame(datalst,columns=[xentry, 'miRNA Class'])
 	sns.violinplot(x='miRNA Class',y=xentry,data=data_master, cut=0)
-	plt.close()	
-	plt.savefig('figures/comp_rel_violin_%s.pdf' %(df_name),bbox_inches='tight')	
+	plt.savefig('figures/comp_rel_violin_%s.pdf' %(df_name),bbox_inches='tight')
+	plt.close()		
 
 
 
