ed678c4caf5888efa66282fcf3a53af814eb35bc
virpatel524 <virpatel3@gmail.com>
2016-06-29 17:43:31 -0500

    update

--- scripts/generate_comparison_mirna_pcg.py
+++ scripts/generate_comparison_mirna_pcg.py
@@ -1,11 +1,16 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import sys
+import csv
 
 def import_pcg():
 	
 
 def import_human_mirages():
+	with open(sys.argv[1],'r') as mir_ages_fle:
+	data = list(csv.reader(mir_ages_fle,delimiter='\t'))
+	print data
+
 
 
 
