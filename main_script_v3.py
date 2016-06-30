661531bcc0ef3a8345abbd51d8e96c33e8f4c6fe
virpatel524 <virpatel3@gmail.com>
2016-06-30 08:20:41 -0500

    update

--- main_script_v3.py
+++ main_script_v3.py
@@ -1190,6 +1190,8 @@ def heatmap_analysis(mirna2age, mirna2disease, mirna2family, gene2age):
 			mirna2family_edited_above15[alpha] = num
 
 
+ 	violin_comp_rel(mirna2family_edited_above15, round_robyn_exp, 'Family', 'Hamming Expression Comparisons', 'fam_tisham_above15')
+
 
 
 
