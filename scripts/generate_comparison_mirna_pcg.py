6a110b7878b9495469def18c97bf53f2c2d2267b
System Administrator <root@Mera-MB.local>
2016-06-29 23:51:25 -0500

    update

--- scripts/generate_comparison_mirna_pcg.py
+++ scripts/generate_comparison_mirna_pcg.py
@@ -97,7 +97,7 @@ fig2 = ax.bar(ind + width, new_bins_gene, width, color='r')
 
 ax.set_ylabel('Count')
 ax.set_xlabel('Ages of miRNAs and Protein-Coding Genes (MY)')
-ax.set_xticks(ind + width)
+ax.set_xticks(ind + width, rotation=65)
 ax.set_xticklabels(str_labels)
 
 sns.plt.show()
