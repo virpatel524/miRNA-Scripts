883b66d9bb1f179154d66db0e08d55d8e70f9d27
virpatel524 <virpatel3@gmail.com>
2016-06-30 14:21:52 -0500

    update

--- main_script_v3.py
+++ main_script_v3.py
@@ -1116,7 +1116,8 @@ def violin_comp_rel_ratio(gen_exlus_dic, hamming_df, tipo, xentry, df_name, new_
 
 
 	for alpha in hamming_df.index:
-		if alpha not 
+		if alpha not in new_df.index:
+			print alpha
 
 
 
