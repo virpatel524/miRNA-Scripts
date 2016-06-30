4555812d59111558b34a1666b834f9d525c4997a
virpatel524 <virpatel3@gmail.com>
2016-06-30 15:13:23 -0500

    update

--- main_script_v3.py
+++ main_script_v3.py
@@ -1123,23 +1123,23 @@ def violin_comp_rel_ratio(gen_exlus_dic, hamming_df, tipo, xentry, df_name, new_
 
 
 
-	# print mean(yes), mean(no)
-	# print median(yes), median(no)
-	# print mannwhitneyu(yes, no)
+	print mean(yes), mean(no)
+	print median(yes), median(no)
+	print mannwhitneyu(yes, no)
 
-	# data_master = pd.DataFrame(datalst,columns=[xentry, 'miRNA Class'])
+	data_master = pd.DataFrame(datalst,columns=[xentry, 'miRNA Class'])
 
-	# if 'tis' in df_name:
-	# 	sns.boxplot(x='miRNA Class',y=xentry,data=data_master)
-	# 	plt.gca().set_ylim([0.0,1.0])
-	# 	plt.savefig('figures/comp_rel_boxplot_%s.pdf' %(df_name),bbox_inches='tight')
-	# 	plt.close()
+	if 'tis' in df_name:
+		sns.boxplot(x='miRNA Class',y=xentry,data=data_master)
+		plt.gca().set_ylim([0.0,1.0])
+		plt.savefig('figures/comp_rel_boxplot_%s.pdf' %(df_name),bbox_inches='tight')
+		plt.close()
 
-	# if 'tar' in df_name:
-	# 	sns.violinplot(x='miRNA Class',y=xentry,data=data_master, cut=0)
-	# 	plt.gca().set_ylim([0.0,0.10])
-	# 	plt.savefig('figures/comp_rel_violinratio_%s.pdf' %(df_name),bbox_inches='tight')
-	# 	plt.close()
+	if 'tar' in df_name:
+		sns.violinplot(x='miRNA Class',y=xentry,data=data_master, cut=0)
+		plt.gca().set_ylim([0.0,0.10])
+		plt.savefig('figures/comp_rel_violinratio_%s.pdf' %(df_name),bbox_inches='tight')
+		plt.close()
 
 
 
@@ -1236,7 +1236,7 @@ def heatmap_analysis(mirna2age, mirna2disease, mirna2family, gene2age):
 	violin_comp_rel_ratio(mirna2family_edited, round_robyn_target, 'Family', 'Hamming Target Comparisons', 'fam_tarham', mir_targetdb)
 
 
-	print 'Hamming, Family, Expression, Above  15'
+	# print 'Hamming, Family, Expression, Above  15'
 
 	mirna2family_edited_above15 = {}
 
