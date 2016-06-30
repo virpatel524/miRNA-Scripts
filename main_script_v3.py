40be15c285d46bcaf8c582012217dc1c6c91475d
System Administrator <root@Mera-MB.local>
2016-06-30 15:55:54 -0500

    update

--- main_script_v3.py
+++ main_script_v3.py
@@ -1103,6 +1103,7 @@ def violin_comp_rel_ratio(gen_exlus_dic, hamming_df, tipo, xentry, df_name, new_
 
 
 	for alpha in hamming_df.index:
+		print alpha
 		for beta in hamming_df.index:
 			if alpha == beta: continue
 			if alpha in genmirtar and beta in genmirtar:
