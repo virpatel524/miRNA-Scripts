5c012f17654c65ad38af4b170d0111019175e434
virpatel524 <virpatel3@gmail.com>
2016-07-12 17:19:27 -0500

    update

--- scripts/generate_combined_figure.py
+++ scripts/generate_combined_figure.py
@@ -51,12 +51,13 @@ def genfig(lst, name, yax, xax, length):
 
 
 	if length == 3:
+		print 'here'
 		tmp = pd.DataFrame(lst, columns=[yax, xax, 'miRNA Class'])
 		sns.violinplot(x=xax, y=yax, cut=0, data=tmp, hue='miRNA Class',  showfliers=False)
 		sns.plt.gca().set_ylim(yaxis_switch(name))
 		sns.plt.savefig('../figures/before_meeting/%s_violin.pdf' %(name), bbox_inches='tight')
 		sns.plt.close()
-
+		print 'there'
 		sns.boxplot(x=xax, y=yax, data=tmp, hue='miRNA Class',  showfliers=False)
 		sns.plt.gca().set_ylim(yaxis_switch(name))
 		sns.plt.savefig('../figures/before_meeting/%s_boxplot.pdf' %(name), bbox_inches='tight')
