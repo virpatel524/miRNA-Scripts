a8d58e823e11e35073d24c77ef837de4759bc5d7
virpatel524 <virpatel3@gmail.com>
2016-07-12 15:58:31 -0500

    update

--- modules/data_import.py
+++ modules/data_import.py
@@ -8,7 +8,64 @@ def parse_disease(disfle):
 	for val in distxt:
 		mirna2disease.setdefault(val[0],[]).append(val[1])
 
-def age_parser(age_txt):
+
+def flatten(l):
+	return [item for sublist in l for item in sublist]
+
+
+def map_relatives(dic):
+	newdic = {}
+
+	for key in dic:
+		for el in dic[key]:
+			newdic[el] = [a for a in dic[key] if a != el]
+
+	return newdic
+
+
+
+def parse_families(txt):
+	mega_mir_lst = []
+	famdict = {}
+	mirlst_by_species = {}
+	human_mirlst = {}
+
+	famlst = [alpha for alpha in list(csv.reader(open(txt),delimiter='\t'))]
+
+
+	current_fam = ''
+
+	for i in famlst:
+		lst = [alpha for alpha in i[0].split(' ') if alpha != '']
+		if i[0][:2] == 'ID' : 
+			current_fam = lst[1]
+		if i[0][:2] == 'MI' :
+			if 'v' in lst[2].split('-')[0]: continue
+			else:
+				famdict.setdefault(current_fam,[]).append(lst[2])
+
+	for key in famdict:
+		new_lst  = [a for a in famdict[key] if 'hsa' in a]
+		if len(new_lst) > 3:
+			human_mirlst[key] = new_lst
+	return human_mirlst 
+
+
+
+
+
+
+
+def parse_disease(disfle):
+	distxt = list(csv.reader(open(disfle), delimiter='\t'))
+	mirna2disease = {}
+
+	for val in distxt:
+		mirna2disease.setdefault(val[0],[]).append(val[1])
+
+	return mirna2disease
+
+def parse_age(agefle):
 	mirna2age = {}
 	age2mirna = {}
 
@@ -20,5 +77,3 @@ def age_parser(age_txt):
 
 
 	return mirna2age
-
-
