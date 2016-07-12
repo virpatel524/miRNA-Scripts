6b262c02c539a3ff1113539e2a5b40b8825929e2
System Administrator <root@Mera-MB.local>
2016-07-12 16:18:53 -0500

    update

--- scripts/generate_combined_figure.py
+++ scripts/generate_combined_figure.py
@@ -50,7 +50,10 @@ def show_jaccard_target():
 			if val not in family_values and secval not in family_values:
 				lstvar = False
 			all_mir.append([round_robyn_target[val][secval], mirna2age[val]])
-			if lstvar == True: family.append([round_robyn_target[val][secval], mirna2age[val]])
+			if lstvar == True: family.append([round_robyn_target[val][secval], mirna2age[val], 'Family'])
+			if lstvar == False: non_family.append([round_robyn_target[val][secval], mirna2age[val], 'Non-Family'])
+
+	
 
 
 
