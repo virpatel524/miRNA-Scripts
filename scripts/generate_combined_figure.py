c9de2167654e50b08672f694bf3d4c68177d7b0b
System Administrator <root@Mera-MB.local>
2016-07-12 22:07:30 -0500

    update

--- scripts/generate_combined_figure.py
+++ scripts/generate_combined_figure.py
@@ -264,7 +264,7 @@ def simple_family_nofamily(mirna2disease, mirna2age,mirna2family, round_robyn_ta
 
 	genfig(join_simple(mod_mirfamval_tarham, mod_mirnofamval_tarham), 'mirbinary_tarham', 'Hamming Targets', 'miRNA Class', 2)
 
-
+	argslst_jac.append(join_simple(mod_mirfamval_tarham, mod_mirnofamval_tarham))
 
 def stratage(mirna2disease, mirna2age,mirna2family, round_robyn_target, round_robyn_exp, mir_expdb, mir_targetdb):
 
