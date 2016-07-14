e9e34584cacc9a4e2ae39faf58b1a492bbfd4dc0
System Administrator <root@Mera-MB.local>
2016-07-13 21:03:51 -0500

    update

---
+++ scripts/combined_family_panels.py
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mirbinary_disjac = parsetxt('../relevant_data/precursor_pd/mirbinary_disjac')
+mirbinary_expjac = parsetxt('../relevant_data/precursor_pd/mirbinary_expjac')
+mirbinary_tarjac = parsetxt('../relevant_data/precursor_pd/mirbinary_tarjac')
+mirbinary_disnum = parsetxt('../relevant_data/precursor_pd/mirbinary_disnum')
+mirbinary_expnum = parsetxt('../relevant_data/precursor_pd/mirbinary_tisnum')
+mirbinary_tarnum = parsetxt('../relevant_data/precursor_pd/mirbinary_tarnum')
+
+mirstrat_disjac = parsetxt('../relevant_data/precursor_pd/mirstrat_disjac')
+mirstrat_expjac = parsetxt('../relevant_data/precursor_pd/mirstrat_expjac')
+mirstrat_tarjac = parsetxt('../relevant_data/precursor_pd/mirstrat_tarjac')
+mirstrat_disnum = parsetxt('../relevant_data/precursor_pd/mirstrat_disnum')
+mirstrat_expnum = parsetxt('../relevant_data/precursor_pd/mirstrat_tisnum')
+mirstrat_tarnum = parsetxt('../relevant_data/precursor_pd/mirstrat_tarnum')
