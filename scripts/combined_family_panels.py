43c124d63117140002749c7c6461b2874d7c9764
virpatel524 <virpatel3@gmail.com>
2016-07-17 13:22:35 -0500

    update

--- scripts/combined_family_panels.py
+++ scripts/combined_family_panels.py
@@ -11,19 +11,19 @@ def genfig():
 
 
 
-mirbinary_disjac = (parsecsvtxt('../relevant_data/precursor_pd/mirbinary_disjac'))
-mirbinary_expjac = parsecsvtxt('../relevant_data/precursor_pd/mirbinary_expjac')
-mirbinary_tarjac = parsecsvtxt('../relevant_data/precursor_pd/mirbinary_tarjac')
-mirbinary_disnum = parsecsvtxt('../relevant_data/precursor_pd/mirbinary_disnum')
-mirbinary_expnum = parsecsvtxt('../relevant_data/precursor_pd/mirbinary_tisnum')
-mirbinary_tarnum = parsecsvtxt('../relevant_data/precursor_pd/mirbinary_tarnum')
+mirbinary_disjac = (parsecsvexport('../relevant_data/precursor_pd/mirbinary_disjac'))
+mirbinary_expjac = parsecsvexport('../relevant_data/precursor_pd/mirbinary_expjac')
+mirbinary_tarjac = parsecsvexport('../relevant_data/precursor_pd/mirbinary_tarjac')
+mirbinary_disnum = parsecsvexport('../relevant_data/precursor_pd/mirbinary_disnum')
+mirbinary_expnum = parsecsvexport('../relevant_data/precursor_pd/mirbinary_tisnum')
+mirbinary_tarnum = parsecsvexport('../relevant_data/precursor_pd/mirbinary_tarnum')
 
-mirstrat_disjac = parsecsvtxt('../relevant_data/precursor_pd/mirstrat_disjac')
-mirstrat_expjac = parsecsvtxt('../relevant_data/precursor_pd/mirstrat_tisjac')
-mirstrat_tarjac = parsecsvtxt('../relevant_data/precursor_pd/mirstrat_tarjac')
-mirstrat_disnum = parsecsvtxt('../relevant_data/precursor_pd/mirstrat_disnum')
-mirstrat_expnum = parsecsvtxt('../relevant_data/precursor_pd/mirstrat_expnum')
-mirstrat_tarnum = parsecsvtxt('../relevant_data/precursor_pd/mirstrat_tarnum')
+mirstrat_disjac = parsecsvexport('../relevant_data/precursor_pd/mirstrat_disjac')
+mirstrat_expjac = parsecsvexport('../relevant_data/precursor_pd/mirstrat_tisjac')
+mirstrat_tarjac = parsecsvexport('../relevant_data/precursor_pd/mirstrat_tarjac')
+mirstrat_disnum = parsecsvexport('../relevant_data/precursor_pd/mirstrat_disnum')
+mirstrat_expnum = parsecsvexport('../relevant_data/precursor_pd/mirstrat_expnum')
+mirstrat_tarnum = parsecsvexport('../relevant_data/precursor_pd/mirstrat_tarnum')
 
 
 
