58a58e948e151161eb0afa41cf5dd89039759a76
virpatel524 <virpatel3@gmail.com>
2016-07-17 15:38:55 -0500

    update

--- scripts/combined_family_panels.py
+++ scripts/combined_family_panels.py
@@ -49,7 +49,8 @@ def genfig1():
 	tmp_tarnum = sorted(mirbinary_tarnum + mirstrat_tarnum, cmp=allages_cmp)
 
 
-	tmp = pd.DataFrame.sort(pd.DataFrame(tmp_disjac, columns=['Jaccard', 'Age (MY)', 'miRNA Class', 'Type']),columns=['Type', 'Age (MY)'])
+	pd_disjac = pd.DataFrame.sort(pd.DataFrame(tmp_disjac, columns=['Jaccard', 'Age (MY)', 'miRNA Class', 'Type']),columns=['Type', 'Age (MY)'])
+
 
 	sns.boxplot(x='Age (MY)', y='Jaccard', hue='miRNA Class', data=tmp,  showfliers=False)
 	sns.plt.show()
