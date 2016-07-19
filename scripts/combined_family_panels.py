6b74e1b92aeba3c9c4e33d8943459c04dcc5e31d
System Administrator <root@Mera-MB.local>
2016-07-18 19:31:12 -0500

    update

--- scripts/combined_family_panels.py
+++ scripts/combined_family_panels.py
@@ -53,14 +53,14 @@ def genfig1():
 	pd_expjac = pd.DataFrame.sort(pd.DataFrame(tmp_expjac, columns=['Jaccard', 'Age (MY)', 'miRNA Class', 'Type']),columns=['Type', 'Age (MY)'])
 	pd_tarjac = pd.DataFrame.sort(pd.DataFrame(tmp_tarjac, columns=['Jaccard', 'Age (MY)', 'miRNA Class', 'Type']),columns=['Type', 'Age (MY)'])
 	
-	f, (ax1, ax2, ax3) = sns.plt.subplots(1, 3, sharey=True , figsize=(75, 15))
+	f, (ax1, ax2, ax3) = sns.plt.subplots(1, 3, sharey=True , figsize=(100, 10))
 
 
 
 	sns.boxplot(x='Age (MY)', y='Jaccard', hue='miRNA Class', data=pd_expjac, showfliers=False, ax=ax1)
 	sns.boxplot(x='Age (MY)', y='Jaccard', hue='miRNA Class', data=pd_tarjac, showfliers=False, ax=ax2)
 	sns.boxplot(x='Age (MY)', y='Jaccard', hue='miRNA Class', data=pd_disjac, showfliers=False, ax=ax3)
-	sns.plt.show()
+	sns.plt.savefig('../figures/disjac_fig.pdf',bbox_inches='tight')
 	sns.plt.close()
 
 
