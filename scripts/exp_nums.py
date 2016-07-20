874d882386a26841773a80e47aff4210c8df4414
System Administrator <root@Mera-MB.local>
2016-07-19 22:13:56 -0500

    update

--- scripts/exp_nums.py
+++ scripts/exp_nums.py
@@ -12,11 +12,14 @@ mirna2age = parse_age('../relevant_data/mirna2age_lst.txt')
 
 newlst = []
 
+family_mirs = flatten(mirna2family.values()) 
+
 famlst = []
 nonfamlst = []
 
 for mirna in mirna2age:
 	if mirna in mir_expdb.index:
 		newlst.append([mirna2age[mirna], sum(mir_expdb.loc[mirna].tolist())])
+		if mirna in 
 
 
