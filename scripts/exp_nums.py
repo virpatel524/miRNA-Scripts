b561daac1512faf5f8177e80e30200b6e4f449e2
System Administrator <root@Mera-MB.local>
2016-07-19 22:06:48 -0500

    update

--- scripts/exp_nums.py
+++ scripts/exp_nums.py
@@ -6,6 +6,8 @@ from general_methods import *
 
 
 mirna2family = parse_families('../relevant_data/miFam.dat')
-mir_expdb = pd.read_csv('/Users/virpatel/Desktop/pub_stuff/relevant_data/exp_data_alldmir.txt', sep='\t',index_col=[0])
+mir_expdb = pd.read_csv('../relevant_data/exp_data_alldmir.txt', sep='\t',index_col=[0])
+mirna2age = parse_age('../relevant_data/mirna2age_lst.txt')
 
-print mir_expdb
+
+print mirna2age
