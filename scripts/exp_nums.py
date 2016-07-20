6310bf0f26deb5fa0abbeed57149ca0195eb059a
System Administrator <root@Mera-MB.local>
2016-07-19 22:01:11 -0500

    update

--- scripts/exp_nums.py
+++ scripts/exp_nums.py
@@ -6,5 +6,6 @@ from general_methods import *
 
 
 mirna2family = parse_families('../relevant_data/miFam.dat')
+mir_expdb = pd.read_csv('/Users/virpatel/Desktop/pub_stuff/relevant_data/exp_data_alldmir.txt', sep='\t',index_col=[0])
 
-print mirna2family
+print mir_expdb
