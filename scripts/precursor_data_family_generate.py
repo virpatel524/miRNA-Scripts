d56147c5961e1bb0514afafab4b0ee64f75299dd
virpatel524 <virpatel3@gmail.com>
2016-07-19 22:38:07 -0500

    update

--- scripts/precursor_data_family_generate.py
+++ scripts/precursor_data_family_generate.py
@@ -209,7 +209,8 @@ def db_gen():
 
 	for alpha in mir_expdb.index:
 		if alpha in mirna2age:
-			if mirna2age[alpha] == 0.0
+			if mirna2age[alpha] == 0.0:
+				print mir_expdb.loc[alpha].tolist()
 
 
 	return mirna2disease, mirna2age, mirna2family, round_robyn_target, round_robyn_exp, mir_expdb, mir_targetdb
