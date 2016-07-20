94a2588535920f6b5047a27765e95f2c38905466
System Administrator <root@Mera-MB.local>
2016-07-19 22:50:47 -0500

    update

--- scripts/precursor_data_family_generate.py
+++ scripts/precursor_data_family_generate.py
@@ -206,9 +206,9 @@ def db_gen():
 
 	for mirna in mirna2age:
 		if mirna in mir_expdb.index:
-			newlst.append([mirna2age[mirna], sum(mir_expdb.loc[mirna].tolist())])
 			if mirna in family_mirs:
-				famlst.append([mirna2age[mirna], sum(mir_expdb.loc[mirna].tolist())])
+				if mirna2age[mirna] == 0.0:
+					print sum(mir_expdb.loc[mirna].tolist())
 			else:
 				nonfamlst.append([mirna2age[mirna], sum(mir_expdb.loc[mirna].tolist())])
 
