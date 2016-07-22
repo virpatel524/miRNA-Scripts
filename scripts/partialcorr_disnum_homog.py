bccd5d6d6dc7aa1a1f9be6bb4ea17c23427f44ea
virpatel524 <virpatel3@gmail.com>
2016-07-22 17:22:18 -0500

    update

--- scripts/partialcorr_disnum_homog.py
+++ scripts/partialcorr_disnum_homog.py
@@ -19,6 +19,7 @@ namesbin = {}
 
 for mirna in disease_jaccard.index:
 	if mirna not in mirna2age: continue
+	if mirna2age[mirna] == 220.2 or mirna2age[mirna] == 324.5: continue
 	namesbin.setdefault(mirna2age[mirna], []).append(mirna)
 
 
