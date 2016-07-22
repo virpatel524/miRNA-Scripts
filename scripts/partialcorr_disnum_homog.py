314b6c40dddac5c32cba2c4a55418de7ba9ec792
virpatel524 <virpatel3@gmail.com>
2016-07-22 16:47:55 -0500

    update

--- scripts/partialcorr_disnum_homog.py
+++ scripts/partialcorr_disnum_homog.py
@@ -20,4 +20,13 @@ for mirna in disease_jaccard.index:
 	if mirna not in mirna2age: continue
 	namesbin.setdefault(mirna2age[mirna], []).append(mirna)
 
-print namesbin
+newlst = []
+
+for age in namesbin.keys():
+	lst_havedone = []
+	for mirna in namesbin[age]:
+		lst_havedone.append(mirna)
+		for secmirna in namesbin[age]:
+			newlst.append([disease_jaccard[mirna][secmirna], age, ])
+
+
