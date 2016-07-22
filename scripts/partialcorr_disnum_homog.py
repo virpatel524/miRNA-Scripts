0af77e1176dcfbb82d5348a0536da07749408ae9
System Administrator <root@Mera-MB.local>
2016-07-22 16:40:40 -0500

    update

--- scripts/partialcorr_disnum_homog.py
+++ scripts/partialcorr_disnum_homog.py
@@ -11,6 +11,6 @@ from general_methods import *
 mirna2disease = parse_disease('../relevant_data/hmdd_database.txt')
 mirna2age = parse_age('../relevant_data/mirna2age_lst.txt')
 
-disease_jaccard = pd.parse_csv('../relevant_data/disease_jaccard_dataframe.txt', sep='\t',index_col=[0])
+disease_jaccard = pd.read_csv('../relevant_data/disease_jaccard_dataframe.txt', sep='\t',index_col=[0])
 
 print disease_jaccard
