5694e3595173eac92aa01aeec588517c21b191fa
System Administrator <root@Mera-MB.local>
2016-07-22 15:51:00 -0500

    update

--- scripts/generate_disease_jaccard.py
+++ scripts/generate_disease_jaccard.py
@@ -3,8 +3,22 @@ from data_import import *
 from general_methods import *
 
 
+def generate_class_vector(biglst, element):
+	new_vec = len(biglst) * [0]
+
+	for alpha in element:
+		new_vec[biglst.index(alpha)] = 1
+
+	return new_vec
+
+
+
 disease_data = parse_disease('../relevant_data/hmdd_database.txt')
 
 diseases_biglst = sorted(list(set(flatten(disease_data.values()))))
 
-print diseases_biglst
+mirna2binary = {}
+
+
+for mirna in disease_data:
+	mirna2binary[mirna] = 
