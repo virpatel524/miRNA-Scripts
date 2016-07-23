52f8f92f621af75d4fb064cc5328983e64be38fc
virpatel524 <virpatel3@gmail.com>
2016-07-23 15:19:03 -0500

    update

--- scripts/expression_number_oldyoung_figure.py
+++ scripts/expression_number_oldyoung_figure.py
@@ -1,5 +1,9 @@
 import seaborn as sns
 import pandas as pd
 import csv
+from data_import import *
+from general_methods import *
 
 
+
+mir_expdb = pd.read_csv('../exp_data_alldmir.txt', sep='\t',index_col=[0])
