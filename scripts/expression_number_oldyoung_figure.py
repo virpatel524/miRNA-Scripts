428d77aadef5b9a4e39220441d6229ed33be6259
System Administrator <root@Mera-MB.local>
2016-07-23 15:22:38 -0500

    update

--- scripts/expression_number_oldyoung_figure.py
+++ scripts/expression_number_oldyoung_figure.py
@@ -4,10 +4,9 @@ import csv
 from data_import import *
 from general_methods import *
 
-mir_expdb = pd.read_csv('../exp_data_alldmir.txt', sep='\t',index_col=[0])
+mir_expdb = pd.read_csv('../relevant_data/exp_data_alldmir.txt', sep='\t',index_col=[0])
 
 mirna2numexp = {}
 
 for alpha in mir_expdb.index:
 	mirna2numexp[alpha] = sum(mir_expdb.loc[alpha].tolist())
-	
