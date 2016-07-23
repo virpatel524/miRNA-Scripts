52f2c2c1880db6ec04cf49c149a6447d8ff6344b
virpatel524 <virpatel3@gmail.com>
2016-07-23 11:23:38 -0500

    update

--- scripts/generate_disease_jaccard.py
+++ scripts/generate_disease_jaccard.py
@@ -12,6 +12,36 @@ def generate_class_vector(biglst, element):
 
 	return new_vec
 
+def collapse_cancer_lst(mirna2disease):
+	gen_classes = ['neopl', 'carc', 'lymph']
+	dis2genclass = {}
+	for dis in flatten(mirna2disease.values()):
+		for val in gen_classes:
+			if val in dis.lower(): 
+				if val not in dis2genclass:
+					dis2genclass[dis] = val
+				continue
+
+
+	mirna2disease_collapsed = {}
+
+
+
+	for mir in mirna2disease:
+		newdislst = []
+		dislst = mirna2disease[mir]
+		for dis in dislst:
+			if dis in dis2genclass.keys():
+				newdislst.append(dis2genclass[dis])
+			else: newdislst.append(dis)
+
+		newdislst = list(set(newdislst))
+
+		mirna2disease_collapsed[mir] = newdislst
+
+	return mirna2disease_collapsed
+
+
 
 
 def generate_matrix(db, str_rel):
@@ -51,7 +81,7 @@ def generate_matrix(db, str_rel):
 
 
 disease_data = parse_disease('../relevant_data/hmdd_database.txt')
-
+disease_data_collapsed = collapse_cancer_lst(disease_data)
 diseases_biglst = sorted(list(set(flatten(disease_data.values()))))
 
 mirna2binary = {}
