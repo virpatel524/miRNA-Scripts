f9a8cf4abe0616926761ea51c6016ed2c6118abd
virpatel524 <virpatel3@gmail.com>
2016-07-23 15:24:54 -0500

    update

--- scripts/expression_number_oldyoung_figure.py
+++ scripts/expression_number_oldyoung_figure.py
@@ -5,9 +5,10 @@ from data_import import *
 from general_methods import *
 
 mir_expdb = pd.read_csv('../relevant_data/exp_data_alldmir.txt', sep='\t',index_col=[0])
-
+mirna2age = parse_age('../relevant_data/mirna2age_lst.txt')
 mirna2numexp = {}
 
 for alpha in mir_expdb.index:
 	mirna2numexp[alpha] = sum(mir_expdb.loc[alpha].tolist())
 
+print mirna2age
