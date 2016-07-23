4922e5f0e4f417540ff1586d254678684e166eac
System Administrator <root@Mera-MB.local>
2016-07-23 18:03:10 -0500

    update

---
+++ scripts/target_youngold_jaccard.py
@@ -0,0 +1,38 @@
+import seaborn as sns
+import pandas as pd 
+import csv
+from jaccard import * 
+from partialcorr import *
+from data_import import * 
+from general_methods import * 
+from scipy.stats import spearmanr, mannwhitneyu
+import numpy as np
+
+target_jaccard = pd.read_csv('../relevant_data/target_heatmap_jaccard_dataframe.txt', sep='\t', index_col=[0])
+mirna2disease = parse_disease('../relevant_data/hmdd_database.txt')
+mirna2age = parse_age('../relevant_data/mirna2age_lst.txt')
+
+
+namesbin = {}
+
+for mirna in target_jaccard.index:
+	if mirna not in mirna2age: continue
+	if mirna2age[mirna] == 220.2 or mirna2age[mirna] == 324.5: continue
+	namesbin.setdefault(mirna2age[mirna], []).append(mirna)
+
+
+newlst = []
+
+for age in namesbin.keys():
+	lst_havedone = []
+	for mirna in namesbin[age]:
+		if mirna not in mir_targetdb.index or mirna not in mir_expdb.index: continue
+		lst_havedone.append(mirna)
+		for secmirna in namesbin[age]:
+			if secmirna not in mir_targetdb.index or secmirna not in mir_expdb.index: continue
+			if secmirna in lst_havedone: continue
+			tarnum = sum(mir_targetdb.loc[mirna].tolist())
+			expnum = sum(mir_expdb.loc[mirna].tolist())
+			tarjac_entry = target_jaccard[mirna][secmirna]
+			expjac_entry = exp_jaccard[mirna][secmirna]
+			newlst.append([target_jaccard[mirna][secmirna], age, len(mirna2disease[mirna]), tarnum, expnum,tarjac_entry, expjac_entry])
