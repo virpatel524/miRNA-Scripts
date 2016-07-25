93885d13439c9143bd16e268de156ac33c9a833e
virpatel524 <virpatel3@gmail.com>
2016-07-24 19:47:37 -0500

    update

--- scripts/find_pvalues.py
+++ scripts/find_pvalues.py
@@ -1,4 +1,3 @@
-import seaborn as sns
 import csv
 from general_methods import *
 
@@ -8,5 +7,11 @@ lst = []
 
 for alpha in data:
 	lst.append(float(alpha[0].split('#')[-1].split('=')[-1].split(')')[0]))
+count = 0
 
-print lst[0]
+for a in lst:
+	if a > 0.05:
+		count += 1
+
+
+print count
