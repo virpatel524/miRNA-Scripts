a7d40fea7e5e3817ca872a9fc16941c8c409cfc7
System Administrator <root@Mera-MB.local>
2016-07-25 20:18:16 -0500

    update

--- scripts/vanelteren.R
+++ scripts/vanelteren.R
@@ -4,3 +4,10 @@ disnum["num"] <- scale(disnum["num"], center = FALSE, scale = max(disnum["num"])
 
 alpha <- sanon(num  ~ grp(class) + strt(age), data = disnum)
 print(summary(alpha))
+
+
+expnum <- read.table("/Users/virpatel/Desktop/pub_stuff/relevant_data/kruskal_data/mirstrat_disnum", header=TRUE, sep=",")
+disnum["num"] <- scale(disnum["num"], center = FALSE, scale = max(disnum["num"]))
+
+alpha <- sanon(num  ~ grp(class) + strt(age), data = disnum)
+print(summary(alpha))
