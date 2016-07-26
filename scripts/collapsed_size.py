eb9408de5a022cd45cc2f963d452c509809b3b98
System Administrator <root@Mera-MB.local>
2016-07-26 10:50:00 -0500

    update

--- scripts/collapsed_size.py
+++ scripts/collapsed_size.py
@@ -1,1 +1,34 @@
 from data_import import *
+
+
+def collapse_cancer_lst(mirna2disease):
+	gen_classes = ['neopl', 'carc', 'lymph']
+	dis2genclass = {}
+	for dis in flatten(mirna2disease.values()):
+		for val in gen_classes:
+			if val in dis.lower(): 
+				if val not in dis2genclass:
+					dis2genclass[dis] = val
+				continue
+
+
+	mirna2disease_collapsed = {}
+
+
+
+	for mir in mirna2disease:
+		newdislst = []
+		dislst = mirna2disease[mir]
+		for dis in dislst:
+			if dis in dis2genclass.keys():
+				newdislst.append(dis2genclass[dis])
+			else: newdislst.append(dis)
+
+		newdislst = list(set(newdislst))
+
+		mirna2disease_collapsed[mir] = newdislst
+
+	return mirna2disease_collapsed
+
+
+mirna2disease = parse_disease('../relevant_data/hmdd_database.txt')
