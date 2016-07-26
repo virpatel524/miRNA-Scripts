cdc747feed1e4a33408cfbfe5d3930919e1f72bb
virpatel524 <virpatel3@gmail.com>
2016-07-25 20:15:46 -0500

    update

--- scripts/vanelteren.R
+++ scripts/vanelteren.R
@@ -1,4 +1,4 @@
-package("sanon")
+library("sanon")
 disnum <- read.table("/Users/virpatel/Desktop/pub_stuff/relevant_data/kruskal_data/mirstrat_disnum", header=TRUE, sep=",")
 disnum["num"] <- scale(disnum["num"], center = FALSE, scale = max(disnum["num"]))
 print(disnum)
