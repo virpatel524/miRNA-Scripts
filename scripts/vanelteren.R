53d72562ed3d8e7d99786ebcb1d8b3b8fdd936bf
virpatel524 <virpatel3@gmail.com>
2016-07-25 20:18:24 -0500

    update

--- scripts/vanelteren.R
+++ scripts/vanelteren.R
@@ -6,8 +6,8 @@ alpha <- sanon(num  ~ grp(class) + strt(age), data = disnum)
 print(summary(alpha))
 
 
-expnum <- read.table("/Users/virpatel/Desktop/pub_stuff/relevant_data/kruskal_data/mirstrat_disnum", header=TRUE, sep=",")
-disnum["num"] <- scale(disnum["num"], center = FALSE, scale = max(disnum["num"]))
+expnum <- read.table("/Users/virpatel/Desktop/pub_stuff/relevant_data/kruskal_data/mirstrat_expnum", header=TRUE, sep=",")
+expnum["num"] <- scale(expnum["num"], center = FALSE, scale = max(disnum["num"]))
 
 alpha <- sanon(num  ~ grp(class) + strt(age), data = disnum)
 print(summary(alpha))
