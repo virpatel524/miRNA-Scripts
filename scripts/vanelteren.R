1f7ce8e4187bbd5458e2fe412e9a3838bc2945f5
virpatel524 <virpatel3@gmail.com>
2016-07-25 20:18:55 -0500

    update

--- scripts/vanelteren.R
+++ scripts/vanelteren.R
@@ -14,7 +14,7 @@ print(summary(alpha))
 
 
 tarnum <- read.table("/Users/virpatel/Desktop/pub_stuff/relevant_data/kruskal_data/mirstrat_tarnum", header=TRUE, sep=",")
-expnum["num"] <- scale(tarnum["num"], center = FALSE, scale = max(expnum["num"]))
+tarnum["num"] <- scale(tarnum["num"], center = FALSE, scale = max(tarnum["num"]))
 
 alpha <- sanon(num  ~ grp(class) + strt(age), data = expnum)
 print(summary(alpha))
